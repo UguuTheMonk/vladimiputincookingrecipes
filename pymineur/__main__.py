c420a1e3fa8f1f1ea41c0227518ff9c1f73c9253
UguuTheMonk <uguuthemonk@gmail.com>
2016-03-17 15:22:35 -0400

    Test3

--- pymineur/__main__.py
+++ pymineur/__main__.py
@@ -2,7 +2,6 @@
 Module principal du package pymineur. C'est ce module que nous allons exécuter pour démarrer votre jeu.
 """
 
-print("Bonjour asdfojaserjhfgvaserfe")
 from pymineur.partie import Partie
 
 if __name__ == '__main__':
